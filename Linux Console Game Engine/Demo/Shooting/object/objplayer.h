14c2e11c328976979ebc4164e29abe26b9dd633c
hoochoona2 <hoochoona2@gmail.com>
2018-02-10 21:32:40 +0900

    Update objplayer.h

--- Linux Console Game Engine/Demo/Shooting/object/objplayer.h
+++ Linux Console Game Engine/Demo/Shooting/object/objplayer.h
@@ -4,16 +4,20 @@
 #include <game_engine/text_graphic.h>
 #include <game_engine/lib/game_engine_lib.h>
 
-#define OBJ_PLAYER  2
+#define OBJ_PLAYER  2 //클래스ID
 
+/**
+* struct objplayer
+* 개인데이터
+**/
 struct objplayer
 {
-    char *key;
-    int cooltime;
-    int ball_speed;
-    int x;
-    int y;
-    tick_t oldtick;
+    char *key;//조작키
+    int cooltime;//공격 쿨타임
+    int ball_speed;//총알 스피드
+    int x;//좌
+    int y;//표
+    tick_t oldtick;//개인데이터
 };
 
 struct object* create_player(int x, int y);
