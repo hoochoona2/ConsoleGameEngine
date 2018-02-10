4f9cc72004db36a26597489d081fa77a58bcc58e
hoochoona2 <hoochoona2@gmail.com>
2018-02-10 22:22:27 +0900

    Update objplayer.c

--- Linux Console Game Engine/Demo/Shooting/object/objplayer.c
+++ Linux Console Game Engine/Demo/Shooting/object/objplayer.c
@@ -1,6 +1,10 @@
 #include "objplayer.h"
 #include <object/objball.h>
 
+/**
+* draw(자신의 객체)
+* 드로잉 함수
+**/
 static void draw(struct object* obj)
 {
     struct objplayer *player = (struct objplayer*)obj->private;
@@ -8,6 +12,10 @@ static void draw(struct object* obj)
     textoutc(obj, player->x-1, player->y, C_RED, 0, "-0-");
 }
 
+/**
+* key_press(자신의 객체, 입력된 키값)
+* z를 누르면 총알이 생성됨
+**/
 static int key_press(struct object *obj, int ch)
 {
     struct objplayer *player = (struct objplayer*)obj->private;
@@ -30,12 +38,15 @@ static int key_press(struct object *obj, int ch)
             player->y++;
             break;
 
+        //공격키, 
         case 'z':
             {
                 tick_t tick = get_tick_count();
 
                 if(player->cooltime <= tick - player->oldtick)
                 {
+                    ///////////////
+                    //총알 생성 및 등록
                     struct object *ball[3] = 
                     {
                         create_ball(player->x-2, player->y-1, player->ball_speed),
@@ -46,6 +57,7 @@ static int key_press(struct object *obj, int ch)
                     int i;
                     for(i=0; i<3; i++)
                         object_add(ball[i]);
+                    ////////////
 
                     player->oldtick = tick;
                 }
@@ -78,18 +90,25 @@ struct object* create_player(int x, int y)
     struct object *obj = (struct object*)lvmv_malloc(&g_mem, sizeof(struct object));
     struct objplayer *player = (struct objplayer*)lvmv_malloc(&g_mem, sizeof(struct objplayer));
 
+    ///////////
+    //클래스 함수 매핑
     obj->draw = draw;
     obj->key_press = key_press;
     obj->ctr = control;
     obj->dst = destroy;
     obj->conf = conflict;
     obj->type = OBJ_PLAYER;
-    player->cooltime = 50;
-    player->ball_speed = 30;
+    ////////////
+
+    //////////
+    //개인 데이터
+    player->cooltime = 50; //쿨타임 50ms
+    player->ball_speed = 30; //총알 스피드 30ms
     player->x = x;
     player->y = y;
     player->oldtick = get_tick_count();
     obj->private = player;
+    ///////////
 
     return obj;
 }
